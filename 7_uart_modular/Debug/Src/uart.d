82e3747a4ff5b71faf80a2392bbf84ec7511801f
Chinmay Bhagwat <156784297+Bhagwat-03@users.noreply.github.com>
2024-10-17 11:16:06 +0530

    Add files via upload

---
+++ 7_uart_modular/Debug/Src/uart.d
@@ -0,0 +1,18 @@
+Src/uart.o: ../Src/uart.c ../Src/uart.h \
+ D:/Bare\ Metal\ Programming/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/Bare\ Metal\ Programming/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ D:/Bare\ Metal\ Programming/CMSIS/Include/core_cm4.h \
+ D:/Bare\ Metal\ Programming/CMSIS/Include/cmsis_version.h \
+ D:/Bare\ Metal\ Programming/CMSIS/Include/cmsis_compiler.h \
+ D:/Bare\ Metal\ Programming/CMSIS/Include/cmsis_gcc.h \
+ D:/Bare\ Metal\ Programming/CMSIS/Include/mpu_armv7.h \
+ D:/Bare\ Metal\ Programming/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Src/uart.h:
+D:/Bare\ Metal\ Programming/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/Bare\ Metal\ Programming/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+D:/Bare\ Metal\ Programming/CMSIS/Include/core_cm4.h:
+D:/Bare\ Metal\ Programming/CMSIS/Include/cmsis_version.h:
+D:/Bare\ Metal\ Programming/CMSIS/Include/cmsis_compiler.h:
+D:/Bare\ Metal\ Programming/CMSIS/Include/cmsis_gcc.h:
+D:/Bare\ Metal\ Programming/CMSIS/Include/mpu_armv7.h:
+D:/Bare\ Metal\ Programming/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ 7_uart_modular/Src/main.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <uart.h>
+#include "stm32f4xx.h"
+
+
+int main(void){
+
+	while(1){
+		printf("Hello from STM32F4........\n\r");
+	}
+}
